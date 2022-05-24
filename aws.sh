5c766a28d649187a33d23ee863f058a82ff1bc8c
Khai Nguyen <khai.nguyen@blinkhealth.com>
2022-05-24 13:09:28 -0700

    Add support for profile name as cli arg; and reset env vars

--- aws.sh
+++ aws.sh
@@ -4,15 +4,29 @@ unset AWS_ACCESS_KEY_ID
 unset AWS_SECRET_ACCESS_KEY
 unset AWS_SESSION_TOKEN
 
-eval $(blink aws session dev)
+# Get CLI arguments
+while getopts p:r option
+do
+    case "$option" in
+        p) profile=${OPTARG};;
+        r) reset=true;;
+    esac
+done
 
-AWS_TOKEN_EXPORTS="/Users/<user.name>/.awstokens"
+# Store AWS token exports
+AWS_TOKEN_EXPORTS="/Users/$(whoami)/.awstokens"
 test -f $AWS_TOKEN_EXPORTS || touch $AWS_TOKEN_EXPORTS
 
 truncate -s 0 $AWS_TOKEN_EXPORTS
 chmod +x $AWS_TOKEN_EXPORTS
 
+if [[ "$reset" = true ]]; then
+    exit
+fi
+
 echo "#!/bin/sh\n" >> $AWS_TOKEN_EXPORTS
-echo "export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID" >> $AWS_TOKEN_EXPORTS
-echo "export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY" >> $AWS_TOKEN_EXPORTS
-echo "export AWS_SESSION_TOKEN=$AWS_SESSION_TOKEN" >> $AWS_TOKEN_EXPORTS
+
+# Start an AWS session
+SESSION="$(blink aws session ${profile:="dev"})"
+echo $SESSION >> $AWS_TOKEN_EXPORTS
+eval "$SESSION"
